a253b320dc416d285c03001a3ca207dd3979b520
Nirmal Kumar <er.kumarnirmal@gmail.com>
2025-04-08 02:51:35 +0530

    first commit

---
+++ src/main/java/com/kumar/submersible_probe/SubmersibleProbeApplication.java
@@ -0,0 +1,13 @@
+package com.kumar.submersible_probe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubmersibleProbeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubmersibleProbeApplication.class, args);
+	}
+
+}
